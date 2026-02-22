4ae61b80476e2355ec9d618838ae56763bf41ea9
Bikash Gosain <bikashgosain@gmail.com>
2026-02-22 23:05:17 +0545

    recreate superuser on each deploy

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ pip install -r requirements.txt
 python manage.py migrate --noinput
 
 # Auto-create superuser if it doesn't exist
+# Auto-create or update superuser
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 import os
@@ -12,7 +13,13 @@ User = get_user_model()
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
-if not User.objects.filter(username=username).exists():
+if User.objects.filter(username=username).exists():
+    u = User.objects.get(username=username)
+    u.set_password(password)
+    u.save()
+    print('Superuser password updated')
+else:
     User.objects.create_superuser(username=username, email=email, password=password)
+    print('Superuser created')
 "
 python manage.py collectstatic --noinput
